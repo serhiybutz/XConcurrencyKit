00a1caa321ef5b9c2c76378dcbfd7794da5459f8
Serhiy Butz <serhiybutz@gmail.com>
2022-11-02 01:18:12 -0400

    Bring everything in line with renaming the account.

--- Sources/XConcurrencyKit/Benchmarking/PreservingMachExecutionTimeMeter+.swift
+++ Sources/XConcurrencyKit/Benchmarking/PreservingMachExecutionTimeMeter+.swift
@@ -2,7 +2,7 @@
 //  PreservingMachExecutionTimeMeter+.swift
 //  XConcurrencyKit
 //
-//  Created by Serge Bouts on 3/29/20.
+//  Created by Serhiy Butz on 3/29/20.
 //  Copyright © 2020 iRiZen.com. All rights reserved.
 //
 
